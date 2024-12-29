aa9ee16ce5b163a5c54e82e3cfccc75fef47b92a
devesh140901 <deveshkumarsingh99@gmail.com>
2024-12-30 01:48:57 +0530

    book show component

--- backend/index.js
+++ backend/index.js
@@ -10,13 +10,14 @@ const app = express();
 app.use(express.json());
 
 // Middleware for handling CORS policy
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
+app.use(cors());
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173/",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
 
 app.get("/", (request, response) => {
   console.log("request", request);
