a52e774d5204e6aacb99f0d9df2171fcd8218606
devesh140901 <deveshkumarsingh99@gmail.com>
2024-12-30 00:36:56 +0530

    Added CORS middleware to backend API

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,22 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import { Book } from "./models/bookModel.js";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 // Middleware to parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS policy
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (request, response) => {
   console.log("request", request);
